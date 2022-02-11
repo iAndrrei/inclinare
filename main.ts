d16a2cf86c7f04c3c3d48e7a0a223a5c619b9194
iAndrrei <97289654+iAndrrei@users.noreply.github.com>
2022-02-11 11:07:06 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        if (input.isGesture(Gesture.TiltRight)) {
+            basic.showArrow(ArrowNames.East)
+        } else if (input.isGesture(Gesture.LogoUp)) {
+            basic.showArrow(ArrowNames.South)
+        } else if (input.isGesture(Gesture.LogoDown)) {
+            basic.showArrow(ArrowNames.North)
+        } else {
+            basic.showIcon(IconNames.Target)
+        }
+    }
+})
